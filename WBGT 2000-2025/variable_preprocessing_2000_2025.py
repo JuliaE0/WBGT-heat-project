986137d49cab3f8d7ac1e992d2bef92b8554ad5c
JuliaE0 <engj3@uci.edu>
2026-02-23 14:14:48 -0800

    set output directory

--- WBGT 2000-2025/variable_preprocessing_2000_2025.py
+++ WBGT 2000-2025/variable_preprocessing_2000_2025.py
@@ -1,4 +1,4 @@
-
+# ---- Variable preprocessing 2000 to 2025 ----
 
 # install required libraries
 # !pip install xarray netCDF4 h5netcdf numpy rioxarray regionmask geopandas
@@ -8,13 +8,14 @@ import numpy as np
 import geopandas as gpd
 import regionmask
 
-# loop over montly files in era5_land_data folder
-
-# save preprocessed files into a new folder
-
+# create folder to hold new preprocessed files
 out_dir = Path("era5_land_preprocessed")
 out_dir.mkdir(parents=True, exist_ok=True)
 
+# loop over monthly files in era5_land_data folder
+IN_DIR = "/Users/new account/Desktop/WBGT-heat-project/WBGT 2000-2025/era5_land_data"
+
+# CONTINUE HERE
 
 # convert t2m from K to Celsius -- used as "Tair" input
 t2m_c = era5land["t2m"] - 273.15
@@ -32,4 +33,22 @@ ssrd_W.attrs["units"] = "W m**-2"
 # convert sp from Pa to hPa -- used as "pres" input
 sp_hpa = era5land["sp"] / 100
 sp_hpa.attrs["units"] = "hPa"
+
+# calculate relative humidity (%) -- used as "relhum" input
+vapor_pres = 610.94*np.exp(17.625*d2m_c / (243.04+d2m_c))
+sat_vapor_pres = 610.94*np.exp(17.625*t2m_c / (243.04+t2m_c))
+rh = 100*(vapor_pres/sat_vapor_pres)
+rh.attrs["long_name"] = "relative humidity (%)"
+rh.attrs["units"] = "%"
+
+# calculate wind speed (m/s) -- used as "speed" input
+u = era5land["u10"]
+v = era5land["v10"]
+ws = np.sqrt(u**2 + v**2)
+ws.attrs["long_name"] = "wind speed (m/s)"
+ws.attrs["units"] = "m s**-1"
+
+
+
+# save preprocessed files into the new folder
 
